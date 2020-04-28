cabe08c1453652bcda8a2e01ad62c83410b42ea4
James Hian Ng <james.ng@projapps.com>
2020-04-28 12:14:25 +0800

    Complete Python Stage #3

--- Coffee Machine/task/machine/coffee_machine.py
+++ Coffee Machine/task/machine/coffee_machine.py
@@ -1,5 +1,26 @@
-cups_of_coffee = int(input("Write how many cups of coffee you will need:"))
-print("For {} cups of coffee you will need:".format(cups_of_coffee))
-print("{} ml of water".format(cups_of_coffee * 200))
-print("{} ml of milk".format(cups_of_coffee * 50))
-print("{} g of coffee beans".format(cups_of_coffee * 15))
+ml_of_water = int(input("Write how many ml of water the coffee machine has:"))
+ml_of_milk = int(input("Write how many ml of milk the coffee machine has:"))
+grams_of_beans = int(input("Write how many grams of coffee beans the coffee machine has:"))
+cups_of_coffee_needed = int(input("Write how many cups of coffee you will need:"))
+
+water_per_cup = 200
+milk_per_cup = 50
+beans_per_cup = 15
+
+cups_of_coffee_made = 0
+
+while water_per_cup <= ml_of_water and \
+        milk_per_cup <= ml_of_milk and \
+        beans_per_cup <= grams_of_beans:
+    cups_of_coffee_made += 1
+    ml_of_water -= water_per_cup
+    ml_of_milk -= milk_per_cup
+    grams_of_beans -= beans_per_cup
+
+if cups_of_coffee_made >= cups_of_coffee_needed:
+    message = "Yes, I can make that amount of coffee"
+    if cups_of_coffee_made > cups_of_coffee_needed:
+        message += " (and even {} more than that)".format(cups_of_coffee_made - cups_of_coffee_needed)
+    print(message)
+else:
+    print("No, I can make only {} cups of coffee".format(cups_of_coffee_made))
